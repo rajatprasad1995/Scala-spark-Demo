99eadfe910f400868f2249b024ab622f2853a1c3
Rajat Prasad <rajat.prasad2@gmail.com>
2021-12-05 00:42:00 +0530

    added batch script

--- run.cmd
+++ run.cmd
@@ -1,2 +1,3 @@
 @echo off
-spark-submit --class com.scalaDemo.App   .\target\pesticide-analysis-1.0-SNAPSHOT-jar-with-dependencies.jar %1
+set arg1=%1
+if "%arg1%"=="" (echo "You have not mentioned the task") else  (spark-submit --class com.scalaDemo.App   .\target\pesticide-analysis-1.0-SNAPSHOT-jar-with-dependencies.jar %arg1%)
